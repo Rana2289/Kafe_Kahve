0a6af556c601ca8e4e33509d86ec27cf075b5f39
ranakaya2289@gmail.com <ranakaya2289@gmail.com>
2023-02-23 14:19:10 +0300

    Proje dosyası ekle.

---
+++ Kafe_Kahve/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kafe_Kahve.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("hakkimizda")]
+        public ActionResult hakkimizda()
+        {
+            return View();
+        }
+        [Route("urunler")]
+        public ActionResult urunler()
+        {
+            return View();
+        }
+        public ActionResult magaza()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kafe_Kahve/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kafe_Kahve.MvcApplication" Language="C#" %>
