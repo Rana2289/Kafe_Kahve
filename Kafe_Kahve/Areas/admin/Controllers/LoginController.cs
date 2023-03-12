2022c07de0eecd11b9c56ad719bb096176eb8e45
ranakaya2289@gmail.com <ranakaya2289@gmail.com>
2023-03-13 00:42:03 +0300

    hata mesajı sayfası

---
+++ Kafe_Kahve/Areas/admin/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kafe_Kahve.Areas.admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: admin/Login
+        public ActionResult Index()
+        {
+            return Content("admin sayfası");
+        }
+    }
+}
